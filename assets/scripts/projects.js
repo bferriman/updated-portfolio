385cc2556faed9e76bf01d2ab04ef1aa3d0839ce
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-11 22:40:49 -0500

    removed most of old portfolio and started fresh

---
+++ assets/scripts/projects.js
@@ -0,0 +1,17 @@
+const projects = [
+{
+    name: "PetSpiration",
+    description: "Are you ready to give up on human companionship but not the fun of a dating app? Then PetSpiration is for you! We'll show you pictures of cats, you tell us whether you like them. Then we'll match you up with adoptable cats near you!",
+    screenshot: "../images/petspiration.jpg"
+},
+{
+    name: "Day Planner",
+    description: "Time to get organized! This app lets you schedule and save events. Uses Moment.js to responsively format time and date, and saves your events in Local Storage. Uses DOM manipulation to display state-appropriate button controls.",
+    screenshot: "../images/dayplanner.jpg"
+},
+{
+    name: "Weather Dashboard",
+    description: "Get the current weather as well as the five day forecast, for wherever you are, or wherever you may be going! Uses the OpenWeather API.",
+    screenshot: "../images/weather.jpg"
+}
+]
